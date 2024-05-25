d20ec28ec5c1ea78fd85f0a314645d445d2670d1
Igor Shaula <igor.shaula@gmail.com>
2024-05-25 23:07:52 +0300

    + setup for the project in IntelliJ Idea

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "org.igor_shaula"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
