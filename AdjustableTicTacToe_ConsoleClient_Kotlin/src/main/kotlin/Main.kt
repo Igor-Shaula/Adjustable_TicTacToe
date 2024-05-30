5f025bd02d077d0a8a887d478356afbe008fbc1d
Igor Shaula <igor.shaula@gmail.com>
2024-05-30 15:48:37 +0300

    simulated real game with new adjusted API in the Kotlin client

--- AdjustableTicTacToe_ConsoleClient_Kotlin/src/main/kotlin/Main.kt
+++ AdjustableTicTacToe_ConsoleClient_Kotlin/src/main/kotlin/Main.kt
@@ -3,9 +3,17 @@ fun main() {
     val field = AtttField(3, 2, 2)
     val rules = AtttRules(3)
     engine.prepare(field, rules)
-    engine.makeNewMove(AtttPlace(0, 0), AtttPlayer.A)
-    engine.makeNewMove(AtttPlace(1, 0), AtttPlayer.A)
-    engine.makeNewMove(AtttPlace(2, 0), AtttPlayer.A)
-    println("engine.gameField = " + engine.getCurrentField())
+
+    engine.makeNewMove(AtttPlace(1, 1))
+    engine.makeNewMove(AtttPlace(0, 0))
+    engine.makeNewMove(AtttPlace(1, 2))
+    engine.makeNewMove(AtttPlace(1, 0))
+    engine.makeNewMove(AtttPlace(2, 0))
+    engine.makeNewMove(AtttPlace(0, 2))
+    engine.makeNewMove(AtttPlace(0, 1))
+    engine.makeNewMove(AtttPlace(2, 1))
+
+    engine.printCurrentFieldIn2d()
+
     println("engine.isRunning() = " + engine.isRunning())
 }
