fa5721c8dba980c59c8d1082e5bd6211d71f4911
Igor Shaula <igor.shaula@gmail.com>
2024-05-31 01:41:17 +0300

    made isGameWon() internal & moved all constants from AtttRules.kt to Limitations.kt

--- AdjustableTicTacToe_Engine_Kotlin/src/main/kotlin/AtttRules.kt
+++ AdjustableTicTacToe_Engine_Kotlin/src/main/kotlin/AtttRules.kt
@@ -12,15 +12,5 @@ class AtttRules(
         else if (winningLength < MIN_WINNING_LINE_LENGTH) winningLength = MIN_WINNING_LINE_LENGTH
     }
 
-    fun isGameWon(lineLength: Int) = lineLength >= winningLength
+    internal fun isGameWon(lineLength: Int) = lineLength >= winningLength
 }
-
-// all numbers & limitations are set here
-const val MIN_GAME_FIELD_SIDE_SIZE = 3 // the game has no sense if less
-const val MAX_GAME_FIELD_SIDE_SIZE = 1000 // because that's already a lot
-const val MIN_WINNING_LINE_LENGTH = 3 // the game has no sense if less
-const val MAX_WINNING_LINE_LENGTH = 30 // even this would be a very complex case
-const val MIN_GAME_FIELD_DIMENSIONS = 2 // classic plain game which is very easy to draw
-const val MAX_GAME_FIELD_DIMENSIONS = 3 // what if we want a 3d? this case can be more complex, but why not? :)
-const val MIN_NUMBER_OF_PLAYERS = 2 // the game has no sense if less
-const val MAX_NUMBER_OF_PLAYERS = 100 // let's imagine that we this engine once works for an online multiplayer game :)
