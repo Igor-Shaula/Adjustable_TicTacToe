e0941a5087b065a9d9868f3a3efbd349ff6b760f
Igor Shaula <igor.shaula@gmail.com>
2024-05-31 10:04:16 +0300

    Engine: NF: just added some descriptions to LineDirection enum values definition

--- AdjustableTicTacToe_Engine_Kotlin/src/main/kotlin/LineDirection.kt
+++ AdjustableTicTacToe_Engine_Kotlin/src/main/kotlin/LineDirection.kt
@@ -3,15 +3,15 @@
  * m -> minus, p -> plus, 0 -> no change along this axis
  */
 internal enum class LineDirection(val dx: Int, val dy: Int) {
-    XmY0(-1, 0),
-    XpY0(+1, 0),
-    X0Ym(0, -1),
-    X0Yp(0, +1),
-    XmYm(-1, -1),
-    XpYp(+1, +1),
-    XmYp(-1, +1),
-    XpYm(+1, -1),
-    None(0, 0);
+    XmY0(-1, 0),    // horizontal to the left
+    XpY0(+1, 0),    // horizontal to the right
+    X0Ym(0, -1),    // vertical up
+    X0Yp(0, +1),    // vertical down
+    XmYm(-1, -1),   // diagonal to left & up
+    XpYp(+1, +1),   // diagonal to right & down
+    XmYp(-1, +1),   // diagonal to left & down
+    XpYm(+1, -1),   // diagonal to right & up
+    None(0, 0);     // no direction
 
     // setting opposite direction is very easy: plus -> minus, minus -> plus, 0 -> 0
     fun opposite(): LineDirection = when (this) {
