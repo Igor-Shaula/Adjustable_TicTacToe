ac290ae02e450957975b4b711b46b41129488504
Igor Shaula <igor.shaula@gmail.com>
2024-06-04 23:41:37 +0300

    LE_Kotlin: lifted AtttPlayer class into its own file

---
+++ Attt_LocalEngine_Kotlin/src/main/kotlin/AtttPlayer.kt
@@ -0,0 +1,4 @@
+// for now, 2 players is enough, but in the future we can have more
+@Suppress("unused")
+enum class AtttPlayer(val symbol: Char) { None('·'), A('X'), B('O') }
+// idea: later make the symbol configurable - because again, we can have more than 2 players in perspective
--- Attt_LocalEngine_Kotlin/src/main/kotlin/SpaceDescriber.kt
+++ Attt_LocalEngine_Kotlin/src/main/kotlin/SpaceDescriber.kt
@@ -11,9 +11,6 @@ internal data object Border : GameSpace
 // endregion WHERE
 // region WHAT
 
-// for now, 2 players is enough, but in the future we can have more
-@Suppress("unused")
-enum class AtttPlayer(val symbol: Char) { None('·'), A('X'), B('O') }
 // idea: later make the symbol configurable - because again, we can have more than 2 players in perspective
 
 // endregion WHAT
