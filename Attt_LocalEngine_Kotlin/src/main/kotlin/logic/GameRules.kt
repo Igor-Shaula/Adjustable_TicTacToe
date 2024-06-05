b1cf1e468160c8fffdaf6e4366d1b94e57c9b09e
Igor Shaula <igor.shaula@gmail.com>
2024-06-05 03:37:31 +0300

    LE_Kotlin: expanded GameRules with getLeadingPlayer() & updatePlayerScore()

--- Attt_LocalEngine_Kotlin/src/main/kotlin/logic/GameRules.kt
+++ Attt_LocalEngine_Kotlin/src/main/kotlin/logic/GameRules.kt
@@ -2,6 +2,8 @@ package logic
 
 import elements.MAX_WINNING_LINE_LENGTH
 import elements.MIN_WINNING_LINE_LENGTH
+import elements.Player
+import publicApi.AtttPlayer
 
 /**
  * internalElements.AtttRules = Adjustable TicTacToe Rules
@@ -11,6 +13,8 @@ internal class GameRules(
     private var winningLength: Int,
     // potentially here we can later add more criteria to detect if the game is won by any of players
 ) {
+    private val players: MutableMap<AtttPlayer, Int> = mutableMapOf()
+
     init {
         // here we're doing possible corrections that may be needed to keep the game rules reasonable
         if (winningLength > MAX_WINNING_LINE_LENGTH) winningLength = MAX_WINNING_LINE_LENGTH
@@ -18,4 +22,17 @@ internal class GameRules(
     }
 
     internal fun isGameWon(lineLength: Int) = lineLength >= winningLength
+
+    internal fun getLeadingPlayer(): AtttPlayer {
+        // here we need the player - not its line length, so do not use maxOfOrNull {...} as it returns Int? in this case
+        val leadingPlayer = players.entries.maxByOrNull { k -> k.value }
+        return leadingPlayer?.key ?: Player.None
+    }
+
+    internal fun updatePlayerScore(whichPlayer: AtttPlayer, newLineLength: Int) {
+        val existingMaxLineLength = players[whichPlayer]
+        if (newLineLength > (existingMaxLineLength ?: 0)) {
+            players[whichPlayer] = newLineLength
+        }
+    }
 }
