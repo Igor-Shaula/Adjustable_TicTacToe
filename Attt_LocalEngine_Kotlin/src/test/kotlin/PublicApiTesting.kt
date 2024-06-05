84381e8928c80e578c556e6351987dd9849a4e21
Igor Shaula <igor.shaula@gmail.com>
2024-06-05 03:40:05 +0300

    LE_Kotlin: updated test3x3FieldWithMultiplePossibleLinesNew() with proper assertion

--- Attt_LocalEngine_Kotlin/src/test/kotlin/PublicApiTesting.kt
+++ Attt_LocalEngine_Kotlin/src/test/kotlin/PublicApiTesting.kt
@@ -1,6 +1,8 @@
+import elements.Player
 import utilities.Log
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 
 // simulation of different cases which can emerge when playing as a consumer of this API
@@ -16,21 +18,20 @@ class PublicApiTesting {
     @Test
     fun test3x3FieldWithMultiplePossibleLinesNew() {
         val game = prepareGameInstanceForClassic3x3GameField()
-
-        // .Xx
-        // .xo
-        // oxo
-
+        /*
+            . X x
+            . x o
+            o x o
+         */
         game.mm(1, 1) // X
         game.mm(2, 1) // O
         game.mm(2, 0) // X
         game.mm(0, 2) // O
         game.mm(1, 2) // X
         game.mm(2, 2) // O
-        game.mm(1, 0) // X - this one was problematic
+        game.mm(1, 0) // X - this one was problematic but in version 0.3.0 this bug was fixed
 
         assertFalse(game.isActive(), "Game should have been won")
-        // Would be nice to be able to do this:
-        // assertEquals(AtttPlayer.A, AtttEngine.getWinner())
+        assertEquals(Player.A, game.getLeader())
     }
 }
