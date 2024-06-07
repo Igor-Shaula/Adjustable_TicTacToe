354d2defb21fe7a6644c923fcdf8f90bfa8535bf
Igor Shaula <igor.shaula@gmail.com>
2024-06-07 21:18:19 +0300

    LE_Kotlin: used prepareNewPlayersInstances() to reset Players' instances for every GameSession

--- Attt_LocalEngine_Kotlin/src/main/kotlin/logic/PlayerProvider.kt
+++ Attt_LocalEngine_Kotlin/src/main/kotlin/logic/PlayerProvider.kt
@@ -5,6 +5,14 @@ import publicApi.AtttPlayer
 
 internal const val SYMBOL_FOR_ABSENT_MARK = '.'
 
+private const val idForPlayerA = 0
+private const val idForPlayerB = 1
+private const val idForPlayerNone = -1
+
+private const val symbolForPlayerA = 'X'
+private const val symbolForPlayerB = 'O'
+private const val symbolForPlayerNone = 'n'
+
 // for now - just the replacement of the former enums use
 internal object PlayerProvider {
     /*
@@ -12,27 +20,17 @@ internal object PlayerProvider {
         val nextAvailableId: Int
             get() = idCounter++
     */
-    val idForPlayerA = 0
-    val idForPlayerB = 1
-    val idForPlayerNone = -1
-
-    val symbolForPlayerA = 'X'
-    val symbolForPlayerB = 'O'
-    val symbolForPlayerNone = 'n'
 
     // this is a very temporary solution to just check the failing tests -> then the architecture will get better
-    val A: AtttPlayer = Player(idForPlayerA, "PlayerA", symbolForPlayerA)
-//        private set
+    var A: AtttPlayer = Player(idForPlayerA, "PlayerA", symbolForPlayerA)
+        private set
     var B: AtttPlayer = Player(idForPlayerB, "PlayerB", symbolForPlayerB)
-//        private set
+        private set
     val None: AtttPlayer = Player(idForPlayerNone, "PlayerNone", symbolForPlayerNone) // one for all cases
 
-    init {
-        prepareNewPlayersInstances()
-    }
-
-    private fun prepareNewPlayersInstances() {
-//        A = Player(idForPlayerA, "PlayerA", symbolForPlayerA)
-//        B = Player(idForPlayerB, "PlayerB", symbolForPlayerB)
+    // creates new instances for all players for every new GameSession instance
+    internal fun prepareNewPlayersInstances() {
+        A = Player(idForPlayerA, "PlayerA", symbolForPlayerA)
+        B = Player(idForPlayerB, "PlayerB", symbolForPlayerB)
     }
 }
