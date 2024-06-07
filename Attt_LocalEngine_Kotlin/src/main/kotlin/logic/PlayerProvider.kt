fdc75b78e7248e3e5c18277b0016128b21d1fd1b
Igor Shaula <igor.shaula@gmail.com>
2024-06-07 21:30:35 +0300

    LE_Kotlin: optimized code in PlayerProvider.kt

--- Attt_LocalEngine_Kotlin/src/main/kotlin/logic/PlayerProvider.kt
+++ Attt_LocalEngine_Kotlin/src/main/kotlin/logic/PlayerProvider.kt
@@ -15,22 +15,20 @@ private const val symbolForPlayerNone = 'n'
 
 // for now - just the replacement of the former enums use
 internal object PlayerProvider {
-    /*
-        private var idCounter = -1
-        val nextAvailableId: Int
-            get() = idCounter++
-    */
 
     // this is a very temporary solution to just check the failing tests -> then the architecture will get better
-    var X: AtttPlayer = Player(idForPlayerX, "PlayerX", symbolForPlayerX)
+    var X: AtttPlayer = createNewPlayerX()
         private set
-    var O: AtttPlayer = Player(idForPlayerO, "PlayerO", symbolForPlayerO)
+    var O: AtttPlayer = createNewPlayerO()
         private set
     val None: AtttPlayer = Player(idForPlayerNone, "PlayerNone", symbolForPlayerNone) // one for all cases
 
     // creates new instances for all players for every new GameSession instance
     internal fun prepareNewPlayersInstances() {
-        X = Player(idForPlayerX, "PlayerX", symbolForPlayerX)
-        O = Player(idForPlayerO, "PlayerO", symbolForPlayerO)
+        X = createNewPlayerX()
+        O = createNewPlayerO()
     }
+
+    private fun createNewPlayerX() = Player(idForPlayerX, "PlayerX", symbolForPlayerX)
+    private fun createNewPlayerO() = Player(idForPlayerO, "PlayerO", symbolForPlayerO)
 }
