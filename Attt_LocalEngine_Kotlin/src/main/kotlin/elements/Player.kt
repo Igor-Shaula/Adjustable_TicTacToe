b26735937f582c1bbd1c4a1862da887613e9acfd
Igor Shaula <igor.shaula@gmail.com>
2024-06-08 11:24:53 +0300

    LE_Kotlin: + definePlayerSymbolById() into Player.kt

--- Attt_LocalEngine_Kotlin/src/main/kotlin/elements/Player.kt
+++ Attt_LocalEngine_Kotlin/src/main/kotlin/elements/Player.kt
@@ -19,4 +19,30 @@ internal data class Player(
     internal fun tryToSetMaxLineLength(newLineLength: Int) {
         if (newLineLength > maxLineLength) maxLineLength = newLineLength
     }
+
+    private fun definePlayerSymbolById(): Char {
+        // let's do it with ASCII table - all following numbers are decimal for simplicity
+        val asciiCode = if (id < 10) {
+            id + '0'.code // 0...9
+        } else if (id < 10 + 26) { // id: 10...35 -> capital letters
+            id + 'A'.code // A...Z
+        } else if (id < 10 + 26 + 26) { // id: 36...61 -> small letters
+            id + 'a'.code // a...z
+        } else if (id < 10 + 26 + 26 + 6) { // id: 62...67 -> signs from ! to & including both
+            id + '!'.code // ! " # $ % &
+        } else if (id < 10 + 26 + 26 + 6 + 6) { // id: 68...73 -> signs from ( to - including both
+            id + '('.code // ( ) * + , -
+        } else if (id < 10 + 26 + 26 + 6 + 6 + 1) { // id = 74 -> sign /
+            id + '/'.code
+        } else if (id < 10 + 26 + 26 + 6 + 6 + 1 + 7) { // id: 75...81 -> signs from : to @
+            id + ':'.code // : ; < = > ? @
+        } else if (id < 10 + 26 + 26 + 6 + 6 + 1 + 7 + 4) { // id: 82...85 -> signs from [ to ^
+            id + '['.code // [ \ ] ^
+        } else if (id < 10 + 26 + 26 + 6 + 6 + 1 + 7 + 4 + 4) { // id: 86...89 -> signs from { to ~
+            id + '{'.code // { | } ~
+        } else { // id: 90+ -> but this case should not be accessible because
+            '_'.code // _
+        }
+        return asciiCode.toChar()
+    }
 }
