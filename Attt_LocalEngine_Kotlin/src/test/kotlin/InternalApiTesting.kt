b8a8ec638760649259258fec9905d5e1c6c4ad1a
Igor Shaula <igor.shaula@gmail.com>
2024-06-08 14:03:54 +0300

    LE_Kotlin: adapted InternalApiTesting to new GameSession constructor and new PlayerProvider API

--- Attt_LocalEngine_Kotlin/src/test/kotlin/InternalApiTesting.kt
+++ Attt_LocalEngine_Kotlin/src/test/kotlin/InternalApiTesting.kt
@@ -28,55 +28,67 @@ class InternalApiTesting {
     // this test was provided by Matt Tucker - https://github.com/tuck182 - many thanks for finding a serious bug!
     @Test
     fun test3x3FieldWithMultiplePossibleLines() { // test name is left as it was in the pull-request
-        val game = GameSession(3, 3)
+        val game = GameSession(3, 3, 2)
+        val playerX = PlayerProvider.playersList[0]
+        val playerO = PlayerProvider.playersList[1]
 
         // .Xx
         // .xo
         // oxo
 
-        game.makeMove(Coordinates(1, 1), PlayerProvider.X)
-        game.makeMove(Coordinates(2, 1), PlayerProvider.O)
-        game.makeMove(Coordinates(2, 0), PlayerProvider.X)
-        game.makeMove(Coordinates(0, 2), PlayerProvider.O)
-        game.makeMove(Coordinates(1, 2), PlayerProvider.X)
-        game.makeMove(Coordinates(2, 2), PlayerProvider.O)
-        game.makeMove(Coordinates(1, 0), PlayerProvider.X) // here A wins and anyway the next possible player is B
+        game.makeMove(Coordinates(1, 1), playerX)
+        game.makeMove(Coordinates(2, 1), playerO)
+        game.makeMove(Coordinates(2, 0), playerX)
+        game.makeMove(Coordinates(0, 2), playerO)
+        game.makeMove(Coordinates(1, 2), playerX)
+        game.makeMove(Coordinates(2, 2), playerO)
+        game.makeMove(Coordinates(1, 0), playerX) // here A wins and anyway the next possible player is B
 
 //        assertFalse(GameEngine.isActive(), "Game should have been won") -> no more relevant as the API was changed
         assertTrue(game.isGameWon(), "Game should have been won")
         // Would be nice to be able to do this:
         // assertEquals(AtttPlayer.A, AtttEngine.getWinner())
         // -> and yes, this is done:
-        assertEquals(PlayerProvider.X, game.getWinner())
+        assertEquals(playerX, game.getWinner())
     }
 
     @Test
     fun having3x3Field_realSimulation2PlayersMovesMade_victoryConditionIsCorrect() {
-        val game = prepareClassic3x3GameField()
-        game.makeMove(Coordinates(0, 0))
-        game.makeMove(Coordinates(1, 0))
-        game.makeMove(Coordinates(2, 0))
-        game.makeMove(Coordinates(1, 1))
-        game.makeMove(Coordinates(2, 1))
-        game.makeMove(Coordinates(1, 2))
+        val game = GameSession(3, 3, 2)
+        game.makeMove(Coordinates(0, 0)) // X
+        game.makeMove(Coordinates(1, 0)) // O
+        game.makeMove(Coordinates(2, 0)) // X
+        game.makeMove(Coordinates(1, 1)) // O
+        game.makeMove(Coordinates(2, 1)) // X
+        game.makeMove(Coordinates(1, 2)) // O - > O wins here
+        /*
+            X O X
+            . O X
+            . O .
+         */
+        game.printCurrentFieldIn2d()
         // gameField & winning message for player B is printed in the console
-        assertEquals(PlayerProvider.O, game.getWinner())
-        assertEquals(PlayerProvider.X, PlayerProvider.activePlayer) // game is ready for the next potential move in any case
+        val playerX = PlayerProvider.playersList[0]
+        val playerO = PlayerProvider.playersList[1]
+        assertEquals(playerO, game.getWinner())
+        assertEquals(playerX, PlayerProvider.activePlayer) // game is ready for the next potential move in any case
         assertEquals(3, game.getWinner().getMaxLineLength())
     }
 
     @Test
     fun having3x3Field_realSimulation2PlayersShortenedMovesMade_victoryConditionIsCorrect() {
-        val game = prepareClassic3x3GameField()
-        game.makeMove(0, 0)
-        game.makeMove(1, 0)
-        game.makeMove(2, 0)
-        game.makeMove(1, 1)
-        game.makeMove(2, 1)
-        game.makeMove(1, 2)
+        val game = GameSession(3, 3, 2)
+        val playerX = PlayerProvider.playersList[0]
+        val playerO = PlayerProvider.playersList[1]
+        game.makeMove(0, 0) // X
+        game.makeMove(1, 0) // O
+        game.makeMove(2, 0) // X
+        game.makeMove(1, 1) // O
+        game.makeMove(2, 1) // X
+        game.makeMove(1, 2) // O
         // gameField & winning message for player B is printed in the console
-        assertEquals(PlayerProvider.O, game.getWinner())
-        assertEquals(PlayerProvider.X, PlayerProvider.activePlayer) // game is ready for the next potential move in any case
+        assertEquals(playerO, game.getWinner())
+        assertEquals(playerX, PlayerProvider.activePlayer) // game is ready for the next potential move in any case
         assertEquals(3, game.getWinner().getMaxLineLength())
     }
 }
