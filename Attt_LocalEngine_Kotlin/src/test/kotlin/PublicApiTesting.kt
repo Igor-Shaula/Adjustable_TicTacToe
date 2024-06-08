5bf9969c6449afc4cabe62c4ed2cc4c93e87b3da
Igor Shaula <igor.shaula@gmail.com>
2024-06-08 14:05:31 +0300

    LE_Kotlin: adapted PublicApiTesting to new GameSession constructor and new PlayerProvider API

--- Attt_LocalEngine_Kotlin/src/test/kotlin/PublicApiTesting.kt
+++ Attt_LocalEngine_Kotlin/src/test/kotlin/PublicApiTesting.kt
@@ -1,4 +1,5 @@
 import logic.PlayerProvider
+import publicApi.AtttGame
 import utilities.Log
 import kotlin.test.BeforeTest
 import kotlin.test.Test
@@ -17,7 +18,7 @@ class PublicApiTesting {
 
     @Test
     fun having3x3Field_onePlayerGetsMultiplePossibleLines_winnerIsDetectedOnceTheConditionsAreMet() {
-        val game = prepareGameInstanceForClassic3x3GameField()
+        val game = AtttGame.create(3, 3)
         /*
             . X x
             . x o
@@ -30,37 +31,40 @@ class PublicApiTesting {
         game.mm(1, 2) // X
         game.mm(2, 2) // O
         game.mm(1, 0) // X - this one was problematic but in version 0.3.0 this bug was fixed
-
+        val playerX = PlayerProvider.playersList[0]
         assertTrue(game.isGameWon(), "Game should have been won")
-        assertEquals(PlayerProvider.X, game.getWinner())
+        assertEquals(playerX, game.getWinner())
     }
 
     @Test
     fun having3x3Field_onePlayerMakesTheFirstLine_leadingPlayerIsDetectedCorrectly() {
-        val game = prepareGameInstanceForClassic3x3GameField()
-        game.mm(0, 0) // A
-        game.mm(1, 0) // B
-        game.mm(0, 1) // A -> now A has a line of 2 marks
-        assertEquals(PlayerProvider.X, game.getLeader())
+        val game = AtttGame.create(3, 3)
+        game.mm(0, 0) // X
+        game.mm(1, 0) // O
+        game.mm(0, 1) // X -> now A has a line of 2 marks
+        val playerX = PlayerProvider.playersList[0]
+        assertEquals(playerX, game.getLeader())
         assertEquals(2, game.getLeader().getMaxLineLength())
     }
 
     @Test
-    fun having5x5Field_onePlayerMakesLongerLineThanAnother_thisPlayerBecomesTheLeadingOne() {
+    fun having4x4Field_onePlayerMakesLongerLineThanAnother_thisPlayerBecomesTheLeadingOne() {
+        val game = AtttGame.create(4, 4)
         /*
             A B A .
             A B . .
             . B . .
             . . . .
          */
-        val game = prepareGameInstanceForClassic3x3GameField()
-        game.mm(0, 0) // A
-        game.mm(1, 0) // B
-        game.mm(0, 1) // A -> now A has a line of 2 marks and becomes a leader
-        game.mm(1, 1) // B -> now B also has a line of 2 marks
-        game.mm(2, 0) // A -> now A still has a line of 2 marks
-        game.mm(1, 2) // A -> now B has a line of 3 marks and becomes a new leader
-        assertEquals(PlayerProvider.O, game.getLeader())
+        game.mm(0, 0) // X
+        game.mm(1, 0) // O
+        game.mm(0, 1) // X -> now A has a line of 2 marks and becomes a leader
+        game.mm(1, 1) // O -> now B also has a line of 2 marks
+        game.mm(2, 0) // X -> now A still has a line of 2 marks
+        game.mm(1, 2) // O -> now B has a line of 3 marks and becomes a new leader
+        val playerO = PlayerProvider.playersList[1]
+        assertEquals(playerO.getId(), game.getLeader().getId())
         assertEquals(3, game.getLeader().getMaxLineLength())
+        game.printCurrentFieldIn2d()
     }
 }
