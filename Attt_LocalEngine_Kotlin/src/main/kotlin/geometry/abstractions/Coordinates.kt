b5062d2d905a9462058ffc5adb6592701c3b2cae
Igor Shaula <igor.shaula@gmail.com>
2024-06-13 10:26:44 +0300

    LE_Kotlin: made Coordinates an abstract class, also added comment line there

--- Attt_LocalEngine_Kotlin/src/main/kotlin/geometry/abstractions/Coordinates.kt
+++ Attt_LocalEngine_Kotlin/src/main/kotlin/geometry/abstractions/Coordinates.kt
@@ -1,9 +1,10 @@
 package geometry.abstractions
 
 /**
- * describes any real position which may exist on the game field
+ * describes any real position which may exist on the game field.
+ * has to be a class because we definitely need many instances of it.
  */
-open class Coordinates(open val x: Int, open val y: Int, open val z: Int = 0) : GameSpace {
+abstract class Coordinates(open val x: Int, open val y: Int, open val z: Int = 0) : GameSpace {
 
     /**
      * detects if given coordinates are correct in the currently active game field
