815eaf36f19d61351e3c1cdbe11ddfcc7a1db52e
Igor Shaula <igor.shaula@gmail.com>
2024-06-15 11:16:33 +0300

    LE_Kotlin: + 3 tests for digits and big & small letters

--- Attt_LocalEngine_Kotlin/src/test/kotlin/PlayerPreparationTests.kt
+++ Attt_LocalEngine_Kotlin/src/test/kotlin/PlayerPreparationTests.kt
@@ -1,8 +1,8 @@
-import constants.MAX_NUMBER_OF_PLAYERS
 import players.PlayerModel
 import utilities.Log
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class PlayerPreparationTests {
 
@@ -11,10 +11,37 @@ class PlayerPreparationTests {
         Log.switch(true)
     }
 
+    //    @Test
+//    fun playerIsCreated_playerSymbolIsDefinedCorrectly() {
+//        (0..MAX_NUMBER_OF_PLAYERS).forEach { index ->
+//            Log.pl("symbol for player with id $index is: ${PlayerModel(index).getSymbol()}")
+//        }
+//    }
+
     @Test
-    fun playerIsCreated_playerSymbolIsDefinedCorrectly() {
-        (0..MAX_NUMBER_OF_PLAYERS).forEach { index ->
-            Log.pl("symbol for player with id $index is: ${PlayerModel(index).getSymbol()}")
+    fun having0players_10playersAreAdded_playersSymbolsAreDefinedCorrectly() {
+        val playersBefore = 0
+        (playersBefore until playersBefore + 10).forEach { i ->
+            assertEquals(('0' + i - playersBefore), PlayerModel(i).getSymbol())
+            Log.pl("Player's index: $i, symbol: ${PlayerModel(i).getSymbol().toString()}")
+        }
+    }
+
+    @Test
+    fun having10players_26playersAreAdded_playersSymbolsAreDefinedCorrectly() {
+        val playersBefore = 10
+        (playersBefore until playersBefore + 26).forEach { i ->
+            assertEquals(('A' + i - playersBefore), PlayerModel(i).getSymbol())
+            Log.pl("Player's index: $i, symbol: ${PlayerModel(i).getSymbol().toString()}")
+        }
+    }
+
+    @Test
+    fun having36players_26playersAreAdded_playersSymbolsAreDefinedCorrectly() {
+        val playersBefore = 10 + 26
+        (playersBefore until playersBefore + 26).forEach { i ->
+            assertEquals(('a' + i - playersBefore), PlayerModel(i).getSymbol())
+            Log.pl("Player's index: $i, symbol: ${PlayerModel(i).getSymbol().toString()}")
         }
     }
 }
