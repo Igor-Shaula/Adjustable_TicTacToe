9ca32b6e80815b5b55de2a9b66513588d519334c
Igor Shaula <igor.shaula@gmail.com>
2024-06-15 12:07:01 +0300

    LE_Kotlin: + constants for previously used symbols number & used them in definePlayerSymbolById()

--- Attt_LocalEngine_Kotlin/src/main/kotlin/players/PlayerModel.kt
+++ Attt_LocalEngine_Kotlin/src/main/kotlin/players/PlayerModel.kt
@@ -2,6 +2,16 @@ package players
 
 import publicApi.AtttPlayer
 
+private const val symbolsBeforeDigits = 0
+private const val symbolsBeforeCapitalLetters = 10
+private const val symbolsBeforeSmallLetters = 10 + 26
+private const val symbolsBefore1stSymbolsBlock = 10 + 26 + 26
+private const val symbolsBefore2ndSymbolsBlock = 10 + 26 + 26 + 6
+private const val symbolsBeforeSlashSymbol = 10 + 26 + 26 + 6 + 6
+private const val symbolsBefore3rdSymbolsBlock = 10 + 26 + 26 + 6 + 6 + 1
+private const val symbolsBefore4thSymbolsBlock = 10 + 26 + 26 + 6 + 6 + 1 + 7
+private const val symbolsBefore5thSymbolsBlock = 10 + 26 + 26 + 6 + 6 + 1 + 7 + 4
+
 /**
  * the replacement of the former enums use, completely describes all player's data
  */
@@ -32,24 +42,24 @@ internal data class PlayerModel(
 
     private fun definePlayerSymbolById(): Char {
         // let's do it with ASCII table - all following numbers are decimal for simplicity
-        val asciiCode = if (id < 10) {
-            id + '0'.code // 0...9
-        } else if (id < 10 + 26) { // id: 10...35 -> capital letters
-            id - (10) + 'A'.code // A...Z
-        } else if (id < 10 + 26 + 26) { // id: 36...61 -> small letters
-            id - (10 + 26) + 'a'.code // a...z
-        } else if (id < 10 + 26 + 26 + 6) { // id: 62...67 -> signs from ! to & including both
-            id - (10 + 26 + 26) + '!'.code // ! " # $ % &
-        } else if (id < 10 + 26 + 26 + 6 + 6) { // id: 68...73 -> signs from ( to - including both
-            id - (10 + 26 + 26 + 6) + '('.code // ( ) * + , -
-        } else if (id < 10 + 26 + 26 + 6 + 6 + 1) { // id = 74 -> sign /
+        val asciiCode = if (id < symbolsBeforeDigits + 10) {
+            id - symbolsBeforeDigits + '0'.code // 0...9
+        } else if (id < symbolsBeforeCapitalLetters + 26) { // id: 10...35 -> capital letters
+            id - symbolsBeforeCapitalLetters + 'A'.code // A...Z
+        } else if (id < symbolsBeforeSmallLetters + 26) { // id: 36...61 -> small letters
+            id - symbolsBeforeSmallLetters + 'a'.code // a...z
+        } else if (id < symbolsBefore1stSymbolsBlock + 6) { // id: 62...67 -> signs from ! to & including both
+            id - symbolsBefore1stSymbolsBlock + '!'.code // ! " # $ % &
+        } else if (id < symbolsBefore2ndSymbolsBlock + 6) { // id: 68...73 -> signs from ( to - including both
+            id - symbolsBefore2ndSymbolsBlock + '('.code // ( ) * + , -
+        } else if (id < symbolsBeforeSlashSymbol + 1) { // id = 74 -> sign /
             '/'.code
-        } else if (id < 10 + 26 + 26 + 6 + 6 + 1 + 7) { // id: 75...81 -> signs from : to @
-            id - (10 + 26 + 26 + 6 + 6 + 1) + ':'.code // : ; < = > ? @
-        } else if (id < 10 + 26 + 26 + 6 + 6 + 1 + 7 + 4) { // id: 82...85 -> signs from [ to ^
-            id - (10 + 26 + 26 + 6 + 6 + 1 + 7) + '['.code // [ \ ] ^
-        } else if (id < 10 + 26 + 26 + 6 + 6 + 1 + 7 + 4 + 4) { // id: 86...89 -> signs from { to ~
-            id - (10 + 26 + 26 + 6 + 6 + 1 + 7 + 4) + '{'.code // { | } ~
+        } else if (id < symbolsBefore3rdSymbolsBlock + 7) { // id: 75...81 -> signs from : to @
+            id - symbolsBefore3rdSymbolsBlock + ':'.code // : ; < = > ? @
+        } else if (id < symbolsBefore4thSymbolsBlock + 4) { // id: 82...85 -> signs from [ to ^
+            id - symbolsBefore4thSymbolsBlock + '['.code // [ \ ] ^
+        } else if (id < symbolsBefore5thSymbolsBlock + 4) { // id: 86...89 -> signs from { to ~
+            id - symbolsBefore5thSymbolsBlock + '{'.code // { | } ~
         } else { // id: 90+ -> but this case should not be accessible because
             '_'.code // _
         }
