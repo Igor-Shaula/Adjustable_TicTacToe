6435fe7eb7736df2936c3fd0645c165e5c12c76d
Igor Shaula <igor.shaula@gmail.com>
2024-06-15 13:28:56 +0300

    CC_Kotlin: + play3x3x3gameWhichIsWon() as a sample for 3D

--- Attt_ConsoleClient_Kotlin/src/main/kotlin/Main.kt
+++ Attt_ConsoleClient_Kotlin/src/main/kotlin/Main.kt
@@ -4,6 +4,8 @@ fun main() {
     play3x3gameWithNoWinner() // 2D
     println()
     play3x3gameWhichIsWon() // 2D
+    println()
+    play3x3x3gameWhichIsWon() // 3D
 }
 
 fun play3x3gameWithNoWinner() {
@@ -45,18 +47,24 @@ fun play3x3gameWhichIsWon() {
     println("game.getWinner() = " + game.getWinner())
 }
 
-    val game2 = AtttGame.create()
-    game2.mm(1, 1) // X
-    game2.mm(2, 1) // O
-    game2.mm(2, 0) // X
-    game2.mm(0, 2) // O
-    game2.mm(1, 2) // X
-    game2.mm(2, 2) // O
-    game2.printCurrentFieldIn2d()
-    println("game2.isGameFinished() = " + game2.isGameFinished())
-    game2.mm(1, 0) // X - this one was problematic
-    game2.printCurrentFieldIn2d()
-    println("game2.isGameFinished() = " + game2.isGameFinished())
-    println("game2.isGameWon() = " + game2.isGameWon())
-    println("game2.getWinner() = " + game2.getWinner())
+fun play3x3x3gameWhichIsWon() {
+    println("=========================")
+    println("game #3 is about to start")
+    val game = AtttGame.create(is3D = true)
+    game.mm(1, 1, 0) // X
+    game.mm(2, 1, 0) // O
+    game.mm(2, 0, 0) // X
+    game.mm(0, 2, 0) // O
+    game.mm(1, 1, 1) // X
+    game.mm(2, 2, 0) // O
+    game.mm(1, 1, 2) // X - this one was problematic but in version 0.3.0 this bug was fixed
+    /*
+        . . x
+        . X o <- x x x - on Z axis
+        o . o
+    */
+    game.printCurrentFieldIn2d()
+    println("game.isGameFinished() = " + game.isGameFinished())
+    println("game.isGameWon() = " + game.isGameWon())
+    println("game.getWinner() = " + game.getWinner())
 }
