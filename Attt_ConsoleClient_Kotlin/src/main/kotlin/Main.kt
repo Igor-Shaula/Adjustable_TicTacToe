cec3cf5794bf9f0f56669a9af065b44eb31b9ed5
Igor Shaula <igor.shaula@gmail.com>
2024-06-15 13:44:03 +0300

    CC_Kotlin: + samples on 4x4 board for 2D & 3D

--- Attt_ConsoleClient_Kotlin/src/main/kotlin/Main.kt
+++ Attt_ConsoleClient_Kotlin/src/main/kotlin/Main.kt
@@ -6,6 +6,10 @@ fun main() {
     play3x3gameWhichIsWon() // 2D
     println()
     play3x3x3gameWhichIsWon() // 3D
+    println()
+    play4x4gameWhichIsWon() // 2D
+    println()
+    play4x4x4gameWhichIsWon() // 3D
 }
 
 fun play3x3gameWithNoWinner() {
@@ -63,6 +67,50 @@ fun play3x3x3gameWhichIsWon() {
         . X o <- x x x - on Z axis
         o . o
     */
+    game.printCurrentFieldIn2d()
+    println("game.isGameFinished() = " + game.isGameFinished())
+    println("game.isGameWon() = " + game.isGameWon())
+    println("game.getWinner() = " + game.getWinner())
+}
+
+fun play4x4gameWhichIsWon() {
+    println("=========================")
+    println("game #4 is about to start")
+    val game = AtttGame.create(4, 3)
+    game.mm(0, 0) // X
+    game.mm(1, 0) // O
+    game.mm(0, 1) // X -> now A has a line of 2 marks and becomes a leader
+    game.mm(1, 1) // O -> now B also has a line of 2 marks
+    game.mm(2, 0) // X -> now A still has a line of 2 marks
+    game.mm(1, 2) // O -> now B has a line of 3 marks and becomes a new leader
+    /*
+        X O X .
+        X O . .
+        . O . .
+        . . . .
+     */
+    game.printCurrentFieldIn2d()
+    println("game.isGameFinished() = " + game.isGameFinished())
+    println("game.isGameWon() = " + game.isGameWon())
+    println("game.getWinner() = " + game.getWinner())
+}
+
+fun play4x4x4gameWhichIsWon() {
+    println("=========================")
+    println("game #5 is about to start")
+    val game = AtttGame.create(4, 3, true)
+    game.mm(0, 0, 0) // X
+    game.mm(1, 0, 0) // O
+    game.mm(0, 1, 0) // X -> now A has a line of 2 marks and becomes a leader
+    game.mm(1, 0, 1) // O -> now B also has a line of 2 marks
+    game.mm(2, 0, 0) // X -> now A still has a line of 2 marks
+    game.mm(1, 0, 2) // O -> now B has a line of 3 marks and becomes a new leader
+    /*
+        a B a . <- b b b . on Z axis
+        a . . .
+        . . . .
+        . . . .
+     */
     game.printCurrentFieldIn2d()
     println("game.isGameFinished() = " + game.isGameFinished())
     println("game.isGameWon() = " + game.isGameWon())
