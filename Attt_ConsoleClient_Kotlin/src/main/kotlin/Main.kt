da6bc27af49bb038476d3d5fc6b2eccd9e5af741
Igor Shaula <igor.shaula@gmail.com>
2024-06-15 13:09:07 +0300

    CC_Kotlin: updated code in main() to use new 0.7.4 LocalEngine library

--- Attt_ConsoleClient_Kotlin/src/main/kotlin/Main.kt
+++ Attt_ConsoleClient_Kotlin/src/main/kotlin/Main.kt
@@ -1,35 +1,40 @@
+import publicApi.AtttGame
+
 fun main() {
-    val field = AtttField(3, 2, 2)
-    val rules = AtttRules(3)
-    val game = AtttGame.create()
-    game.prepare(field, rules)
+    println("=======================")
+    println("game1 is about to start")
 
-    game.mm(1, 1)
-    game.mm(0, 0)
-    game.mm(1, 2)
-    game.mm(1, 0)
-    game.mm(2, 0)
-    game.mm(0, 2)
-    game.mm(0, 1)
-    game.mm(2, 1)
-    game.mm(2, 2)
+    val game1 = AtttGame.create()
+    game1.mm(1, 1)
+    game1.mm(0, 0)
+    game1.mm(1, 2)
+    game1.mm(1, 0)
+    game1.mm(2, 0)
+    game1.mm(0, 2)
+    game1.mm(0, 1)
+    game1.mm(2, 1)
+    game1.mm(2, 2)
+    game1.printCurrentFieldIn2d()
+    println("game1.isGameFinished() = " + game1.isGameFinished())
+    println("game1.isGameWon() = " + game1.isGameWon())
+    println("game1.getWinner() = " + game1.getWinner())
+    println("game1.getLeader() = " + game1.getLeader())
 
-    game.printCurrentFieldIn2d()
+    println("\n=======================")
+    println("\ngame2 is about to start")
 
-    println("engine.isActive() = " + game.isActive())
-    game.finish()
-    println("engine.isActive() = " + game.isActive())
-
-    game.prepare(field, rules)
-    game.prepare(AtttField(3), AtttRules(3))
-    game.mm(1, 1) // X
-    game.mm(2, 1) // O
-    game.mm(2, 0) // X
-    game.mm(0, 2) // O
-    game.mm(1, 2) // X
-    game.mm(2, 2) // O
-    println("engine.isActive() = " + game.isActive())
-    game.printCurrentFieldIn2d()
-    game.mm(1, 0) // X - this one was problematic
-    println("engine.isActive() = " + game.isActive())
+    val game2 = AtttGame.create()
+    game2.mm(1, 1) // X
+    game2.mm(2, 1) // O
+    game2.mm(2, 0) // X
+    game2.mm(0, 2) // O
+    game2.mm(1, 2) // X
+    game2.mm(2, 2) // O
+    game2.printCurrentFieldIn2d()
+    println("game2.isGameFinished() = " + game2.isGameFinished())
+    game2.mm(1, 0) // X - this one was problematic
+    game2.printCurrentFieldIn2d()
+    println("game2.isGameFinished() = " + game2.isGameFinished())
+    println("game2.isGameWon() = " + game2.isGameWon())
+    println("game2.getWinner() = " + game2.getWinner())
 }
