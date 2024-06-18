978c8f910564ea7689a0ee375f35afd2ed02f4b2
Igor Shaula <igor.shaula@gmail.com>
2024-06-19 00:13:09 +0300

    LE_Kotlin: + Javadoc for all new kinds of create() factory method

--- Attt_LocalEngine_Kotlin/src/main/kotlin/publicApi/AtttGame.kt
+++ Attt_LocalEngine_Kotlin/src/main/kotlin/publicApi/AtttGame.kt
@@ -32,24 +32,42 @@ interface AtttGame {
             desiredPlayerNumber: Int = MIN_NUMBER_OF_PLAYERS
         ): AtttGame = GameSession(desiredFieldSize, desiredMaxLineLength, is3D, desiredPlayerNumber)
 
+        /**
+         * the shortest ever way to get the game ready
+         */
         fun create(): AtttGame =
             GameSession(MIN_GAME_FIELD_SIDE_SIZE, MIN_WINNING_LINE_LENGTH, false, MIN_NUMBER_OF_PLAYERS)
 
+        /**
+         * convenient for getting the game field of bigger size
+         */
         fun create(desiredFieldSize: Int = MIN_GAME_FIELD_SIDE_SIZE): AtttGame =
             GameSession(desiredFieldSize, MIN_WINNING_LINE_LENGTH, false, MIN_NUMBER_OF_PLAYERS)
 
+        /**
+         * more precise adjustment of winning condition for configurable game field
+         */
         fun create(
             desiredFieldSize: Int = MIN_GAME_FIELD_SIDE_SIZE,
             desiredMaxLineLength: Int = MIN_WINNING_LINE_LENGTH
         ): AtttGame =
             GameSession(desiredFieldSize, desiredMaxLineLength, false, MIN_NUMBER_OF_PLAYERS)
 
+        /**
+         * the shortest ever way to get the game ready in 3D
+         */
         fun create(is3D: Boolean = false): AtttGame =
             GameSession(MIN_GAME_FIELD_SIDE_SIZE, MIN_WINNING_LINE_LENGTH, is3D, MIN_NUMBER_OF_PLAYERS)
 
+        /**
+         * convenient for getting the game field of bigger size in 3D
+         */
         fun create(desiredFieldSize: Int = MIN_GAME_FIELD_SIDE_SIZE, is3D: Boolean = false): AtttGame =
             GameSession(desiredFieldSize, MIN_WINNING_LINE_LENGTH, is3D, MIN_NUMBER_OF_PLAYERS)
 
+        /**
+         * more precise adjustment of winning condition for configurable 3D game field
+         */
         fun create(
             desiredFieldSize: Int = MIN_GAME_FIELD_SIDE_SIZE,
             desiredMaxLineLength: Int = MIN_WINNING_LINE_LENGTH,
