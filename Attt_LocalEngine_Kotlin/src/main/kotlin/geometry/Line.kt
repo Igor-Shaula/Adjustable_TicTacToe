50475ff23addaa56883adcbc1807c0c5dd065e2d
Igor Shaula <igor.shaula@gmail.com>
2024-06-24 11:15:53 +0300

    LE_Kotlin: updated API of Line - enabled equals() & hashCode() again, and based them onto using "marks"

--- Attt_LocalEngine_Kotlin/src/main/kotlin/geometry/Line.kt
+++ Attt_LocalEngine_Kotlin/src/main/kotlin/geometry/Line.kt
@@ -18,7 +18,7 @@ internal class Line(startingMark: Coordinates, adjacentMark: Coordinates) {
      * also I decided to avoid using LineDirection concept here - for simplicity.
      * all control of line correctness is done at the level of line detection inside existing logic chains.
      */
-    val marks: MutableSet<Coordinates> = mutableSetOf()
+    internal val marks: MutableSet<Coordinates> = mutableSetOf()
 
     init {
         marks.add(startingMark)
@@ -29,22 +29,21 @@ internal class Line(startingMark: Coordinates, adjacentMark: Coordinates) {
         marks.add(newMark)
     }
 
-    /*
-        // it is decided to treat two lines with similar marks but opposite direction as one (the same) line
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            return if (other == null || other !is Line) false
-            else this.startingMark == other.adjacentMark && this.adjacentMark == other.startingMark
-        }
+    // it is decided to treat two lines with similar marks but opposite direction as one (the same) line
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        return if (other == null || other !is Line) false
+        else marks == other.marks // as we use unordered set - it will be the same for opposite line as well
+    }
 
-        // if we override equals -> we have to override hashCode as well to prevent from breaking their contract
-        override fun hashCode(): Int {
-            return (startingMark.z + adjacentMark.z) * 1_000_000 + // 1 & 1 -> 2_000_000
-                    (startingMark.y + adjacentMark.y) * 1_000 + // 1 & 1 -> 2_002_000
-                    (startingMark.x + adjacentMark.x) // 1 & 1 -> 2_002_002
-            // this is only beginning implementation - later check upper limits & maybe include lineDirection here
-        }
-    */
+    // if we override equals -> we have to override hashCode as well to prevent from breaking their contract
+    override fun hashCode(): Int {
+        return marks.hashCode() // as a line is a set of marks - let recognize it by the set of marks
+    }
+
+    override fun toString(): String {
+        return marks.toString()
+    }
 }
 
 internal fun Set<Line?>.getMaxLength(): Int =
