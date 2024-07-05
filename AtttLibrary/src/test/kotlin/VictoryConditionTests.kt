697eef2e198569bb9885dac2fdc554f41c6c9734
Igor Shaula <igor.shaula@gmail.com>
2024-07-06 00:49:17 +0300

    made tests to private chosenAlgorithm

--- AtttLibrary/src/test/kotlin/VictoryConditionTests.kt
+++ AtttLibrary/src/test/kotlin/VictoryConditionTests.kt
@@ -20,9 +20,9 @@ class VictoryConditionTests {
     fun having3x3Field_onlyOnePlayerMarksAreSet_victoryConditionIsCorrect() {
         val game = Game.create() as GameSession
         val playerX = PlayerProvider.playersList[0]
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(0, 0), playerX)
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(1, 0), playerX)
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(2, 0), playerX)
+        game.makeMove(game.gameField.getCoordinatesFor(0, 0), playerX)
+        game.makeMove(game.gameField.getCoordinatesFor(1, 0), playerX)
+        game.makeMove(game.gameField.getCoordinatesFor(2, 0), playerX)
         // gameField & winning message for player A is printed in the console
         assertEquals(playerX, game.getWinner())
         assertEquals(MIN_WINNING_LINE_LENGTH, game.getWinner().maxLineLength)
@@ -37,13 +37,13 @@ class VictoryConditionTests {
         val game = Game.create(5, 5) as GameSession
         val playerX = PlayerProvider.playersList[0]
         Log.pl("\ntest5x5Field: gameEngine ready with given field: ${game.gameField.prepareForPrinting3dIn2d()}")
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(0, 0), playerX)
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(1, 0), playerX)
+        game.makeMove(game.gameField.getCoordinatesFor(0, 0), playerX)
+        game.makeMove(game.gameField.getCoordinatesFor(1, 0), playerX)
         // GameEngine.makeNewMove(Coordinates(2, 0), WhichPlayer.A) // intentionally commented - it will be used a bit later
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(3, 0), playerX)
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(4, 0), playerX)
+        game.makeMove(game.gameField.getCoordinatesFor(3, 0), playerX)
+        game.makeMove(game.gameField.getCoordinatesFor(4, 0), playerX)
         // intentionally placed here to connect 2 segments
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(2, 0), playerX)
+        game.makeMove(game.gameField.getCoordinatesFor(2, 0), playerX)
         assertEquals(playerX, game.getWinner())
         assertEquals(5, game.getWinner().maxLineLength)
         assertEquals(PlayerModel.None, PlayerProvider.activePlayer)
@@ -63,14 +63,14 @@ class VictoryConditionTests {
         // .xo
         // oxo
 
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(1, 1), playerX)
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(2, 1), playerO)
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(2, 0), playerX)
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(0, 2), playerO)
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(1, 2), playerX)
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(2, 2), playerO)
+        game.makeMove(game.gameField.getCoordinatesFor(1, 1), playerX)
+        game.makeMove(game.gameField.getCoordinatesFor(2, 1), playerO)
+        game.makeMove(game.gameField.getCoordinatesFor(2, 0), playerX)
+        game.makeMove(game.gameField.getCoordinatesFor(0, 2), playerO)
+        game.makeMove(game.gameField.getCoordinatesFor(1, 2), playerX)
+        game.makeMove(game.gameField.getCoordinatesFor(2, 2), playerO)
         game.makeMove(
-            game.gameField.chosenAlgorithm.getCoordinatesFor(1, 0),
+            game.gameField.getCoordinatesFor(1, 0),
             playerX
         ) // here A wins and anyway the next possible player is B
 
@@ -89,12 +89,12 @@ class VictoryConditionTests {
     @Test
     fun having3x3Field_realSimulation2PlayersMovesMade_victoryConditionIsCorrect() {
         val game = Game.create() as GameSession
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(0, 0)) // X
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(1, 0)) // O
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(2, 0)) // X
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(1, 1)) // O
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(2, 1)) // X
-        game.makeMove(game.gameField.chosenAlgorithm.getCoordinatesFor(1, 2)) // O - > O wins here
+        game.makeMove(game.gameField.getCoordinatesFor(0, 0)) // X
+        game.makeMove(game.gameField.getCoordinatesFor(1, 0)) // O
+        game.makeMove(game.gameField.getCoordinatesFor(2, 0)) // X
+        game.makeMove(game.gameField.getCoordinatesFor(1, 1)) // O
+        game.makeMove(game.gameField.getCoordinatesFor(2, 1)) // X
+        game.makeMove(game.gameField.getCoordinatesFor(1, 2)) // O - > O wins here
         /*
             X O X
             . O X
